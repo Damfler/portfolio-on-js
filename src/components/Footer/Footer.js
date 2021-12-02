c2482a849cfbcfc32c1a7a9d9ec028d619faf049
Dima Gashuk <72968420+Damfler@users.noreply.github.com>
2021-12-02 17:41:49 +0300

    update: Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,50 @@
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import {AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillYoutube} from 'react-icons/ai';
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import {SocialIcons} from '../Header/HeaderStyles';
+import {
+    CompanyContainer,
+    FooterWrapper,
+    LinkColumn,
+    LinkItem,
+    LinkList,
+    LinkTitle,
+    Slogan,
+    SocialContainer,
+    SocialIconsContainer
+} from './FooterStyles';
 
 const Footer = () => {
-  return (
-    <div>
-      Footer
-    </div>
-  );
+    return (
+        <FooterWrapper>
+            <LinkList>
+                <LinkColumn>
+                    <LinkTitle>Call</LinkTitle>
+                    <LinkItem href='tel:111-111-1111'>111-111-1111</LinkItem>
+                </LinkColumn>
+                <LinkColumn>
+                    <LinkTitle>Email</LinkTitle>
+                    <LinkItem href='mailto:gashuk2002@yandex.ru'>gashuk2002@yandex.ru</LinkItem>
+                </LinkColumn>
+            </LinkList>
+            <SocialIconsContainer>
+                <CompanyContainer>
+                    <Slogan>Innovating one project at a time</Slogan>
+                </CompanyContainer>
+                <SocialContainer>
+                    <SocialIcons href='https://github.com/Damfler'>
+                        <AiFillGithub size='3rem'/>
+                    </SocialIcons>
+                    <SocialIcons href='https://www.instagram.com/damfler/'>
+                        <AiFillInstagram size='3rem'/>
+                    </SocialIcons>
+                    <SocialIcons href='https://www.youtube.com/channel/UCWIbaZ6Nt-pRcRie3g8SqZw'>
+                        <AiFillYoutube size='3rem'/>
+                    </SocialIcons>
+                </SocialContainer>
+            </SocialIconsContainer>
+        </FooterWrapper>
+    );
 };
 
 export default Footer;
